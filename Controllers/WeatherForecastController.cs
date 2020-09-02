3f5fbf200a306a32f8a5b63c326b60e84b524f12
Deepak Kumar <deepak.kumar11@nagarro.com>
2020-09-02 21:46:51 +0530

    Project Added

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RedisDemo.Service;
+
+namespace RedisDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ICacheService cacheService;
+
+
+        [HttpGet("cache/{key}")]
+        public async Task<ActionResult<string>> GetCache(string key)
+        {
+            return await cacheService.GetCachedResponseAsync(key);
+        }
+
+        [HttpPost("cache/{key}/{value}")]
+        public async Task<ActionResult> SetCache(string key, string value)
+        {
+            await cacheService.CacheResponseAsync(key, value, TimeSpan.FromSeconds(5000));
+            return Ok();
+        }
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, ICacheService cacheService)
+        {
+            _logger = logger;
+            this.cacheService = cacheService;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
